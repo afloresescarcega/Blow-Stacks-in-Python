f297acd1f51ad1b6832c6adaedf627145417bf33
Alex Flores <mirnaescarcega@Alexs-iMac.local>
2017-08-08 12:39:19 -0500

    Finished stack class with restrictions on size

---
+++ stack.py
@@ -0,0 +1,34 @@
+
+
+class Stack:
+    """
+
+    [btm of stack, top of stack]
+
+    """
+
+    def __init__():
+        self._size = 0
+        self.con = []
+
+    def isEmpty(self):
+        return self.con == []
+
+    def push(self, item):
+        if(size() < 2):
+            self.con.append(item)
+        else:
+            raise(ValueError)
+
+    def pop(self):
+        return self.con.pop()
+
+    def peek(self):
+        return self.con[size() - 1]
+
+    def size(self):
+        return len(self.con)
+
+
+
+    
