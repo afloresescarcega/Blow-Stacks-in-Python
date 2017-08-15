6f944b4e23aac64563233cb4488b6e1496a88b43
Alex Flores <afloresescarcega@utexas.edu>
2017-08-14 21:36:52 -0500

    Add lack of self when calling size in stack class in stack.py

--- stack.py
+++ stack.py
@@ -7,15 +7,19 @@ class Stack:
 
     """
 
-    def __init__():
-        self._size = 0
+    def __init__(self):
+        self.size = 0
         self.con = []
+
+    def __init__(self, con):
+        self.size = 0
+        self.con = con
 
     def isEmpty(self):
         return self.con == []
 
     def push(self, item):
-        if(size() < 2):
+        if(self.get_size() < 2):
             self.con.append(item)
         else:
             raise(ValueError)
@@ -24,13 +28,16 @@ class Stack:
         return self.con.pop()
 
     def peek(self):
-        return self.con[size() - 1]
+        return self.con[self.size() - 1]
 
-    def size(self):
+    def get_size(self):
         return len(self.con)
     
     def peekAll(self):
         return self.con # [btm, top]
+
+    def __str__(self):
+        return str(self.con)
 
 
 
